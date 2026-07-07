894c738745b7a0bc258db8122cd112c74989084e
BDestroyerOfWorlds <busra06200420@gmail.com>
2026-07-08 02:01:29 +0300

    first ever version

--- main.py
+++ main.py
@@ -1,52 +1,45 @@
 import funcs
-
-
-print("""
-lazyctl - quick and easy wifi/bluetooth control
-
-Commands:
-    bluetooth -r    Restart bluetooth
-    bluetooth -on   Turn bluetooth on
-    bluetooth -off  Turn bluetooth off
-    wifi -r         Restart wifi
-    wifi -on        Turn wifi on
-    wifi -off       Turn wifi off
-""")
-choice = input(">")
-
-if choice == "bluetooth -r":
-    funcs.bt_restart()
-
-elif choice == "bluetooth -on":
-    funcs.bt_on()
-
-elif choice == "bluetooth -off":
-    funcs.bt_off()
-
-elif choice == "wifi -r":
-    funcs.wifi_restart()
-
-elif choice == "wifi -on":
-    funcs.wifi_on()
-
-elif choice == "wifi -off":
-    funcs.wifi_off()
-
-
-
-
-else:
+import time
+while True:
     print("""
-lazyctl - quick and easy wifi/bluetooth control
+    lazyctl - quick and easy wifi/bluetooth control
+    
+    Commands:
+        bluetooth -r    Restart bluetooth
+        bluetooth -on   Turn bluetooth on
+        bluetooth -off  Turn bluetooth off
+        wifi -r         Restart wifi
+        wifi -on        Turn wifi on
+        wifi -off       Turn wifi off
+        exit            exit application
+    """)
+    choice = input(">")
 
-Commands:
-    bluetooth -r    Restart bluetooth
-    bluetooth -on   Turn bluetooth on
-    bluetooth -off  Turn bluetooth off
-    wifi -r         Restart wifi
-    wifi -on        Turn wifi on
-    wifi -off       Turn wifi off
-""")
+    if choice == "bluetooth -r":
+        funcs.bt_restart()
+
+    elif choice == "bluetooth -on":
+        funcs.bt_on()
+
+    elif choice == "bluetooth -off":
+        funcs.bt_off()
+
+    elif choice == "wifi -r":
+        funcs.wifi_restart()
+
+    elif choice == "wifi -on":
+        funcs.wifi_on()
+
+    elif choice == "wifi -off":
+        funcs.wifi_off()
+
+    elif choice == "exit":
+        break
+
+    else:
+        print("Invalid command")
+        time.sleep(1)
+
 
 
 
