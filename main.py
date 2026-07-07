b6894d082b6aa8dbe16e9d1074565ba17321d3e6
BDestroyerOfWorlds <busra06200420@gmail.com>
2026-07-08 01:55:18 +0300

    first ever version

--- main.py
+++ main.py
@@ -32,6 +32,25 @@ elif choice == "wifi -on":
 elif choice == "wifi -off":
     funcs.wifi_off()
 
+
+
+
+else:
+    print("""
+lazyctl - quick and easy wifi/bluetooth control
+
+Commands:
+    bluetooth -r    Restart bluetooth
+    bluetooth -on   Turn bluetooth on
+    bluetooth -off  Turn bluetooth off
+    wifi -r         Restart wifi
+    wifi -on        Turn wifi on
+    wifi -off       Turn wifi off
+""")
+
+
+
+
 """
 CONSIDER ARGPARSE, TYPER, CLICK, DYKES
 Typer and dykes require type annotations to work. Argparse and click do not.
